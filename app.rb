621305ba4d0d119c49eba1e63dcb31e906c79646
Samuel Banya <sbanya@fastmail.com>
2022-08-17 18:26:57 -0400

    Adding latest changes to assignment

---
+++ app.rb
@@ -0,0 +1,27 @@
+# app.rb
+puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+
+print "Pass this test, please."
+
+# print "\n"
+
+# Example in which Ruby already uses .to_s() to convert the output to a string:
+# puts [1, 2, 3]
+
+# p method, which uses .inspect() on the array itself:
+p [1, 2, 3]
+
+# Using the 'pp' (pretty print) method:
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# NOTE: If you want to use Ruby interactively, use 'irb' for a Ruby REPL (read evaluate print loop)
+
+# NOTE: The 'puts' method always returns 'nil'
+
+# NOTE: You can exit 'irb' with the 'exit' keyword or 'ctrl + d'
